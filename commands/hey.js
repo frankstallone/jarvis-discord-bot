1fd93f25742d9fbeba7e94fdc7e32becac1c713b
Frank Stallone <frank@stallone.dev>
2020-03-21 10:49:15 -0400

    init: 🎉 Initial commit for JARVIS

---
+++ commands/hey.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "hey",
+  description: "JARVIS's hello",
+  execute(message, args) {
+    message.reply(
+      `Howdy! I am Just A Rather Very Intelligent System (J.A.R.V.I.S.)`
+    );
+  }
+};
