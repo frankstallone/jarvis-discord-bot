a4331fc85419acbb475d3ec646adce7ebd72cf4c
Frank Stallone <frank@stallone.dev>
2020-03-23 15:43:41 -0400

    docs: 📝 Cleaner docs

--- src/commands/index.js
+++ src/commands/index.js
@@ -4,6 +4,7 @@ const hey = require('./hey')
 const version = require('./version')
 const whois = require('./whois')
 
+// Similar to TypeScript Enums 😃
 const commands = {
     clear,
     hey,
@@ -11,15 +12,21 @@ const commands = {
     whois
 };
 
+const prefix = '!';
 const guild_id = process.env.GUIDE_ID;
 
 module.exports = async (msg) => {
-    if (msg.channel.guild.id === guild_id) {
-        const args = msg.content.split(' ');        
-        // Check to ensure ther are arguments && a ! prefix
-        if (args.length == 0 || args[0].charAt(0) !== '!') return;
+    // Prefix required, no bots
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
+    if (msg.channel.guild.id === guild_id) {
+        // Split up arguments
+        const args = msg.content.split(' ');
+        // Check to ensure there are arguments && a ! prefix
+        if (args.length == 0 || args[0].charAt(0) !== '!') return;
+        // Grab command, remove bang
         const command = args.shift().substr(1);
+        // Enum check for valid command, fire command
         if (Object.keys(commands).includes(command)) {
             commands[command](msg, args);
         }
