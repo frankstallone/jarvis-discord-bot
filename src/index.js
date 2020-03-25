ad970b9c22da4575a496ebcc43c614724ff61782
Frank Stallone <frank@stallone.dev>
2020-03-25 13:29:07 -0400

    feat: ✨ Added prettier & welcome message to bot

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 require('dotenv').config();
@@ -12,8 +12,18 @@ const commandHandler = require('./commands');
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
  * received from Discord
  */
-client.on("ready", () => {
+client.on('ready', () => {
   console.log(`🦾 ${client.user.tag} is online!`);
+});
+
+// Welcome message
+client.on('guildMemberAdd', member => {
+  // #bot channel ID
+  const channel = member.guild.channels.cache.find(channel => channel.id === '690859062136406077')
+  if(!channel) return;
+
+  channel.send(`Welcome to Stallone's Speakeasy, ${member}! 🥂 Be sure to check out the <#691991371556454410> channel.`)
+
 });
 
 // Create an event listener for messages
