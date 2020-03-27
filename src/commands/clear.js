8af6e5eca62490ed08f2454a62ca6b6fa4877442
Frank Stallone <frank@stallone.dev>
2020-03-27 15:47:02 -0400

    feat: ✨ Added giphy to clear err msg

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,6 +1,12 @@
+const Discord = require('discord.js');
 module.exports = async (msg, args) => {
   // Not everyone can clear
-  if (!msg.member.hasPermission("MANAGE_MESSAGES")) return;
+  if (!msg.member.hasPermission("MANAGE_MESSAGES")) { 
+    const giphy = new Discord.MessageEmbed()
+      .setTitle(`🚨 Error: You do not have the right permissions to clear messages`)
+      .setImage('https://media.giphy.com/media/9jb1IK331xkGs/giphy.gif')
+    return msg.channel.send(giphy)
+  };
   !args[0]
     ? await msg.reply(`🚨 Error: Second argument required`)
     : await msg.channel
